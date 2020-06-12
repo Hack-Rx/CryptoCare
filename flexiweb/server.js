7259559361fa8ca220a43b54b73463c30551d434
Shobit Puri <shobitpuri2@gmail.com>
2020-06-12 16:34:09 +0530

    Added boomerang ui assets and made a login page

--- flexiweb/server.js
+++ flexiweb/server.js
@@ -2,7 +2,16 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+//Routing 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-app.listen(3001);
+app.get('/signin', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/login.html'));
+});
+
+
+app.listen(3002);
 
 app.use(express.static('public'))
