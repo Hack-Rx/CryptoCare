e7e91be96be6e6b67a0e73521453cd1854294086
Shobit Puri <shobitpuri2@gmail.com>
2020-06-12 18:31:15 +0530

    Added Doctor Dashboard

--- flexiweb/server.js
+++ flexiweb/server.js
@@ -14,10 +14,18 @@ app.get('/signin', function(req, res) {
 app.get('/Patient', function(req, res) {
     res.sendFile(path.join(__dirname + '/pages/patientinitial.html'));
 });
-app.get('/Patient/Dash', function(req, res) {
+
+app.get('/Patient/JPg9A2/Dash', function(req, res) {
     res.sendFile(path.join(__dirname + '/pages/patientdash.html'));
 });
 
+app.get('/Patient/JPg9A2/Teleform', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/teleform.html'));
+});
+
+app.get('/Doctor/Dash', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/doctordash.html'));
+});
 app.listen(3002);
 
 app.use(express.static('public'))
